85ebd30a9d674a21a0963ccf4f3ea14442ea02aa
jonny0702 <jonathanvergararuiz@gmail.com>
2022-11-08 20:50:35 -0500

    :white_check_mark: add: Intial dependencies and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import Web3 from "web3";
 
 function App() {
+
+  useEffect(()=>{
+    if(window.ethereum){
+      // window.ethereum.request({
+      //   method: 'eth_requestAccounts',
+      // }).then((accounts: any) => console.log(accounts))
+
+      const web = new Web3(window.ethereum);
+      web.eth.requestAccounts().then(console.log)
+    }
+  },[]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </HashRouter>
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
